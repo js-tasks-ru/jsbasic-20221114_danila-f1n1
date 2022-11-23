b0f74b2e92d4a41486913a938135dc650f3911d1
Danila <danila.f1n1@yandex.ru>
2022-11-23 15:45:39 +0300

    исправил

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,8 +1,5 @@
 function checkSpam(str) {
   let newStr = str.toLowerCase();
-  if ( newStr.includes('xxx') || newStr.includes('1xbet')) {
-    return true
-  } else {
-    return false
-  }
+
+  return newStr.includes('xxx') || newStr.includes('1xbet')
 }
