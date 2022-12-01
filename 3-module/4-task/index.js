763831edf3eb836c0be944fec521448d2fa40f33
Danila <danila.f1n1@yandex.ru>
2022-12-01 18:17:30 +0300

    3 module done

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,6 @@
 function showSalary(users, age) {
-  // ваш код...
+  let newArr = users.filter(user => user.age <= age).map(user => `${user.name}, ${user.balance}`)
+  for(let i = 0 ; i <= newArr.length ; i++) {
+    return ((i !== newArr.length) ? newArr.join('\n') : newArr.join('') )
+  }
 }
--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,12 @@
 function getMinMax(str) {
-  // ваш код...
+  let newArr = str.split(' ').filter(item => isFinite(item)).map(item => item).sort((a, b) => a - b)
+  min = Number(newArr[0])
+  max = Number(newArr[newArr.length -1])
+  let result = {
+    min: min,
+    max: max
+  }
+  console.log(typeof min)
+  console.log(newArr)
+  return result
 }
