a8a27d7031f88abc19b380c789f13bdb9bf595c9
Danila <danila.f1n1@yandex.ru>
2022-12-05 15:18:29 +0300

    module 4 done

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,35 +1,7 @@
-// function highlight(table) {
-//
-//   const list = document.querySelectorAll('tbody');
-//
-//   for (const item of list) {
-//     const trList = item.rows;
-//
-//     for (const node of trList) {
-//       const tdArr = node.cells;
-//
-//       for (const cell of tdArr) {
-//         // Выделение занят/не занят
-//         if (!cell.hasAttribute('data-available')) {
-//           node.setAttribute('hidden', '');
-//         } else {
-//           cell.dataset.available === 'true' ? node.classList.add('available') : node.classList.add('unavailable');
-//           node.removeAttribute('hidden');
-//         }
-//         //   Проверка пола
-//         node.cells[2].innerHTML === 'm' ? node.classList.add('male') : node.classList.add('female');
-//         //   Проверка возраста
-//         node.cells[1].innerHTML < 18 ? node.style.textDecoration = 'line-through' : '';
-//       }
-//     }
-//   }
-// }
 const FIRST_COLUMN = 1;
 const SECOND_COLUMN = 2;
 const THIRD_COLUMN = 3;
-
 function highlight(table) {
-  // ваш код...
   const actions = {
     [THIRD_COLUMN]: (root, td) => {
       if (td.dataset.available === 'true') {
