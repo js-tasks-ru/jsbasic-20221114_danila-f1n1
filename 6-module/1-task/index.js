5c3f4b4bbe4b41123ea697273b32f0d12d82a1fc
Danila <danila.f1n1@yandex.ru>
2022-12-18 11:28:36 +0300

    module 6 done

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -12,7 +12,48 @@
  *      }
  *
  */
+
+
 export default class UserTable {
   constructor(rows) {
+    this.rows = rows;
+    this.render();
+    this.elem.addEventListener('click', this.#onButtonClick);
+  }
+
+  render() {
+    const container = document.createElement('table');
+    container.innerHTML = `
+    <table>
+      <thead>
+          <tr>
+            <th>Имя</th>
+            <th>Возраст</th>
+            <th>Зарплата</th>
+            <th>Город</th>
+            <th></th>
+          </tr>
+      </thead>
+      <tbody>
+        ${this.rows.map(data => `
+        <tr>
+            <td>${data.name}</td>
+            <td>${data.age}</td>
+            <td>1${data.salary}</td>
+            <td>${data.city}</td>
+            <td><button data-action="remove">X</button></td>
+        </tr>
+        `).join('')}
+      </tbody>
+    </table>
+    `;
+    this.elem = container;
+  }
+
+  #onButtonClick = (e) => {
+    if (e.target.dataset.action !== 'remove') {
+      return;
+    }
+    e.target.closest('tr').remove();
   }
 }
